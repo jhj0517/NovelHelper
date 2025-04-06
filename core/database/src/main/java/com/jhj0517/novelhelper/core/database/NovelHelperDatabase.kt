a4787fdc4909898348371b5602c17bc77c1afc50
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2025-04-06 23:32:02 +0900

    refactor: remove deprecated version/section

--- core/database/src/main/java/com/jhj0517/novelhelper/core/database/NovelHelperDatabase.kt
+++ core/database/src/main/java/com/jhj0517/novelhelper/core/database/NovelHelperDatabase.kt
@@ -8,19 +8,13 @@ import androidx.room.TypeConverters
 import com.jhj0517.novelhelper.core.database.converter.LocalDateTimeConverter
 import com.jhj0517.novelhelper.core.database.dao.BranchDao
 import com.jhj0517.novelhelper.core.database.dao.DocumentDao
-import com.jhj0517.novelhelper.core.database.dao.SectionDao
-import com.jhj0517.novelhelper.core.database.dao.VersionDao
 import com.jhj0517.novelhelper.core.database.entity.BranchEntity
 import com.jhj0517.novelhelper.core.database.entity.DocumentEntity
-import com.jhj0517.novelhelper.core.database.entity.SectionEntity
-import com.jhj0517.novelhelper.core.database.entity.VersionEntity
 
 @Database(
     entities = [
         DocumentEntity::class,
-        BranchEntity::class,
-        VersionEntity::class,
-        SectionEntity::class
+        BranchEntity::class
     ],
     version = 1,
     exportSchema = false
@@ -29,8 +23,6 @@ import com.jhj0517.novelhelper.core.database.entity.VersionEntity
 abstract class NovelHelperDatabase : RoomDatabase() {
     abstract fun documentDao(): DocumentDao
     abstract fun branchDao(): BranchDao
-    abstract fun versionDao(): VersionDao
-    abstract fun sectionDao(): SectionDao
 
     companion object {
         private const val DATABASE_NAME = "novelhelper.db"
